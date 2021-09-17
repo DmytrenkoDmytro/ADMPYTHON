aaa1260006fd8de9e6e780770be0438b07f69b4b
Dmytro Dmytrenko <dmytrenko.d.a@gmail.com>
2021-09-17 16:49:05 +0300

    2 Sample

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-#Use OS comand
+
 import subprocess
-subprocess.call(["dir"])
+
+#Use DOS/Win comand (for linux the same)
+subprocess.call(["dir","C:","/w"])
+print("\n")
+
+#Info about system(linux)
+#command 1
+uname ="uname"
+uname_arg ="-a"
+print("Gathering system info with %s command:\n" % uname)
+subprocess.call([uname, uname_arg])
+#command 2
+diskspace = "df"
+diskspace_arg = "-h"
+print("\n Gathering disk space info with %s command:\n" % diskspace)
+subprocess.call([diskspace, diskspace_arg])
