6cedd6a616a0cbb171e3a3c2703d1a449e80ee7a
Dmytro Dmytrenko <dmytrenko.d.a@gmail.com>
2021-09-20 09:00:49 +0300

    Set Function module

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 import subprocess
 
 #Use DOS/Win comand (for linux the same)
-subprocess.call(["dir","C:","/w"])
-print("\n")
+def osco(c,a,p)
+    subprocess.call([c,a,p])
+    print("\n")
 
 #Info about system(linux)
-#command 1
-uname ="uname"
-uname_arg ="-a"
-print("Gathering system info with %s command:\n" % uname)
-subprocess.call([uname, uname_arg])
-#command 2
-diskspace = "df"
-diskspace_arg = "-h"
-print("\n Gathering disk space info with %s command:\n" % diskspace)
-subprocess.call([diskspace, diskspace_arg])
+def osinfo()
+    #command 1
+    uname ="uname"
+    uname_arg ="-a"
+    print("Gathering system info with %s command:\n" % uname)
+    subprocess.call([uname, uname_arg])
+    #command 2
+    diskspace = "df"
+    diskspace_arg = "-h"
+    print("\n Gathering disk space info with %s command:\n" % diskspace)
+    subprocess.call([diskspace, diskspace_arg])
